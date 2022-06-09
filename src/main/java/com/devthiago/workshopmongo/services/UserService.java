cddb916e4e7667e4bc1afdd25ee804f8252fc77f
DevThiagoLuiz <thiagoluiz5123@gmail.com>
2022-06-09 11:18:40 -0300

    getting a user by id

--- src/main/java/com/devthiago/workshopmongo/services/UserService.java
+++ src/main/java/com/devthiago/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.devthiago.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devthiago.workshopmongo.domain.User;
 import com.devthiago.workshopmongo.repository.UserRepository;
+import com.devthiago.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,13 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user.orElseThrow(() ->  new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
 }
